4cd4043463be3f143c32f44281261f6a75fdc35a
Cristhian <https://github.com/cristhianrave>
2022-03-08 23:28:12 +0100

    add card

--- src/App_Ecommerce/settings.py
+++ src/App_Ecommerce/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'cart',
     'crispy_forms',
 ]
 
--- src/App_Ecommerce/urls.py
+++ src/App_Ecommerce/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('', include('cart.urls')),
 ]
 
 
